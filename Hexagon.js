a85e0ca4e6a9239a7cb8ac72462abebfdf650a7f
shamelj <70355802+shamelj@users.noreply.github.com>
2022-04-14 18:09:43 +0300

    temp
    random

--- Hexagon.js
+++ Hexagon.js
@@ -1,4 +1,6 @@
-let inintialColor = '#eef5a6', playerColor = 'blue',aiColor = 'red';
+let inintialColor = '#eef5a6',
+    playerColor = 'blue',
+    aiColor = 'red';
 class Hexagon {
     constructor() {
         this.vertices = [];
@@ -7,53 +9,75 @@ class Hexagon {
         this.initializeEdges();
     }
     initializeVertices() {
-        const margin =10
+        const margin = 10
         // added in clockwise order starting from top
-        this.vertices.push(new Vertex(width/2, margin)); // top
+        this.vertices.push(new Vertex(width / 2, margin)); // top
         this.vertices.push(new Vertex(width - margin, height / 3.0)); // top right
         this.vertices.push(new Vertex(width - margin, height * 2 / 3.0)); // bottom right
-        this.vertices.push(new Vertex(width/2, height - margin)); // bottom
+        this.vertices.push(new Vertex(width / 2, height - margin)); // bottom
         this.vertices.push(new Vertex(margin, height * 2 / 3.0)); // bottom left
         this.vertices.push(new Vertex(margin, height / 3.0)); // top left
-        console.log(this.vertices)
         this.renderVertices();
     }
-    renderVertices(){
+    renderVertices() {
         for (let vertex of this.vertices)
             vertex.show();
     }
     initializeEdges() {
-        for (let i = 0;i<6;i++){
-            this.edges.push([0,0,0,0,0,0]);
+        for (let i = 0; i < 6; i++) {
+            this.edges.push([0, 0, 0, 0, 0, 0]);
         }
-        console.log(this.edges)
         this.showEdges()
     }
-    showEdges(){
-        for (let i = 0;i<this.edges.length;i++)
-            for (let j = i+1;j<this.edges[i].length;j++)
-                this.showEdge(i,j,inintialColor);
-        
+    showEdges() {
+        for (let i = 0; i < this.edges.length; i++)
+            for (let j = i + 1; j < this.edges[i].length; j++)
+                this.showEdge(i, j, inintialColor);
+
     }
-    showEdge(i,j,color){
-        let vertex1 = {x: this.vertices[i].x,y: this.vertices[i].y};
-        let vertex2 = {x: this.vertices[j].x,y: this.vertices[j].y};
+    showEdge(i, j, color) {
+        let vertex1 = {
+            x: this.vertices[i].x,
+            y: this.vertices[i].y
+        };
+        let vertex2 = {
+            x: this.vertices[j].x,
+            y: this.vertices[j].y
+        };
         strokeWeight(1)
         stroke(color)
-        line(vertex1.x,vertex1.y,vertex2.x,vertex2.y)
+        line(vertex1.x, vertex1.y, vertex2.x, vertex2.y)
     }
-     getVertex(x,y){
-        for (let i=0;i<this.vertices.length;i++)
-            if (dist(x,y,this.vertices[i].x,this.vertices[i].y) <=10)
-            return i;
+    getVertex(x, y) {
+        for (let i = 0; i < this.vertices.length; i++)
+            if (dist(x, y, this.vertices[i].x, this.vertices[i].y) <= 10)
+                return i;
         return -1;
-      }
-      addEdge(src,dest){
-        if (this.edges[src][dest]!=0 || this.edges[dest][src]!=0)
+    }
+    addEdge(src, dest, submitter) {
+        if (this.edges[src][dest] != 0 || this.edges[dest][src] != 0)
             return false;
-        this.edges[src][dest] = turn;
-        this.edges[dest][src] = turn;
-        this.showEdge(src,dest,'blue')
+        this.edges[src][dest] = submitter;
+        this.edges[dest][src] = submitter;
+        this.showEdge(src, dest, submitter == ai ? aiColor : playerColor)
         return true;
-      }
+    }
+    removeEdge(src, dest) {
+        this.edges[src][dest] = 0;
+        this.edges[dest][src] = 0;
+        this.showEdge(src, dest, inintialColor)
+
+    }
+    triangleIsformed(vertex) {
+        const n = this.edges[vertex].length
+        for (let i = 0; i < n; i++) {
+            if (i == vertex || this.edges[vertex][i] == 0) continue;
+            for (let j = i + 1; j < n; j++)
+                if (this.edges[vertex][j] == this.edges[vertex][i] && this.edges[i][j] == this.edges[vertex][i]) {
+                    //console.log(this.edges, this.edges[vertex][i])
+                    return true;
+                }
+        }
+        return false;
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,39 +1,126 @@
-let hexagon;
-let player = 0,
-  ai = 1,
-  turn = 0,
+let hexagon // = new Hexagon();;
+let player = 1,
+  ai = 2,
+  turn = 1,
+  finished = 0,
   source = -1,
   target = -1;
+
 
 function setup() {
   let side = min(windowHeight, windowWidth) * 0.9
   createCanvas(side, side);
   background(220);
   hexagon = new Hexagon();
+  hexagon.addEdge(0, 1, ai);
 }
 
-
 function draw() {
+  //if (turn == ai)
+   // aiTurn()
 
 
 
 
+}
 
+function playerTurn(vertex) {
+  if (source == -1 || vertex == source) {
+    source = vertex;
+    return;
+  }
+  target = vertex;
+  let isAdded = hexagon.addEdge(source, target, player);
+  if (hexagon.triangleIsformed(target))
+    turn = finished;
+  else
+    turn = ai;
+  source = -1;
+  target = -1;
 }
 
 function mousePressed() {
   let vertex = hexagon.getVertex(mouseX, mouseY);
-  if (vertex == -1)
+  if (vertex == -1 || turn == finished)
     return;
-  if (source==-1){
-    source= vertex;
+  playerTurn(vertex);
+  if (turn == player || turn == finished)
     return;
-  }
-  target = vertex;
-  let isAdded = hexagon.addEdge(source,target);
-  if (!isAdded){
-    source =-1
-    target=-1;
-  }
+  aiTurn();
 
+
+}
+
+function aiTurn() {
+
+  let bestScore = -Infinity;
+  let move;
+  for (let i = 0; i < hexagon.edges.length; i++) {
+    for (let j = i + 1; j < hexagon.edges.length; j++) {
+      if (hexagon.edges[i][j] == 0) {
+        hexagon.addEdge(i, j, ai);
+        let score = hexagon.triangleIsformed(i) ? -1 : miniMax(20, bestScore, Infinity, false)
+        hexagon.removeEdge(i, j);
+        if (score > bestScore) {
+          bestScore = score;
+          move = {
+            i,
+            j
+          };
+
+        }
+      }
+      console.log(bestScore,i,j)
+      if (bestScore == 1) break;
+    }
+    if (bestScore == 1) break;
+  }
+  hexagon.addEdge(move.i, move.j, ai);
+  if (hexagon.triangleIsformed(move.i))
+    turn = finished;
+  else
+    turn = player;
+}
+
+function miniMax(depth, alpha, beta, isMaximizing) {
+  let bestScore;
+  if (isMaximizing) {
+    if (depth == 0) return Infinity;
+    bestScore = -Infinity;
+    for (let i = 0; i < hexagon.edges.length; i++) {
+      for (let j = i + 1; j < hexagon.edges[i].length; j++) {
+        if (hexagon.edges[i][j] == 0) {
+          hexagon.addEdge(i, j, ai);
+          let score = hexagon.triangleIsformed(i) ? -1 : miniMax(depth - 1, alpha, beta, false)
+          hexagon.removeEdge(i, j);
+          bestScore = max(bestScore, score);
+          alpha = max(alpha, score)
+          if (beta <= alpha || bestScore >0)
+            return bestScore;
+        }
+        
+
+      }
+
+    }
+  } else {
+    if (depth == 0) return -Infinity;
+    bestScore = Infinity;
+    for (let i = 0; i < hexagon.edges.length; i++) {
+      for (let j = i + 1; j < hexagon.edges.length; j++) {
+        if (hexagon.edges[i][j] == 0) {
+          hexagon.addEdge(i, j, player);
+          let score = hexagon.triangleIsformed(i) ? 1 : miniMax(depth - 1, alpha, beta, true)
+          hexagon.removeEdge(i, j);
+          bestScore = min(bestScore, score);
+          beta = min(beta, score)
+          if (beta <= alpha || bestScore <0)
+            return bestScore;
+        }
+      }
+
+    }
+
+  }
+  return bestScore;
 }
